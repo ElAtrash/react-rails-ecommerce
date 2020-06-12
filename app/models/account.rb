f1add586c3ad1b50b9a49dd6d33c347fc163bc59
ElAtrash <bioelatrash@gmail.com>
2020-06-12 17:08:25 +0300

    setup devise

---
+++ app/models/account.rb
@@ -0,0 +1,21 @@
+class Account < ApplicationRecord
+  enum role: [:User, :Manager,:Admin]
+  has_many :orders
+  validates :role, presence: true
+  validates :email, presence: true, uniqueness: true
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def admin?
+    self.role == 'Admin'
+  end
+
+  def manager?
+    self.role == 'Manager'
+  end
+
+  def user?
+    self.role == 'User'
+  end
+end
