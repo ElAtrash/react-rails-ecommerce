19a78b73644b69c829362140563962a775e6fab5
ElAtrash <bioelatrash@gmail.com>
2020-06-30 16:15:39 +0300

    add filter scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ApplicationRecord
   has_many :cart_items
 
   validates :title, :description, :image_url, :price, presence: true
+
+  scope :filter_by_title, -> (title) { where("title like ?", "#{title}%")}
 end
