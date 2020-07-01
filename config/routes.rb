15d101b89d3f1cc08b29408e948801194fdc8080
ElAtrash <bioelatrash@gmail.com>
2020-07-01 22:25:28 +0300

    fix devise signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :accounts
+  namespace :api do
+    namespace :v1 do
+      resources :categories
+      resources :products
+      
+      get 'categories/index'
+      get '/categories/:id', to: 'categories#show'
+
+      get 'products/index'
+      get '/products/:id', to: 'products#show'
+      resources :carts
+      resources :cart_items
+    end
+  end
+  get 'accounts/check_for_account', to: 'accounts#check_for_account'
+
+  devise_for :accounts do
+    get '/accounts/sign_out' => 'devise/sessions#destroy'
+  end
+  resources :accounts
   root "home#index"
+  get '/*path' => 'home#index'
 end
