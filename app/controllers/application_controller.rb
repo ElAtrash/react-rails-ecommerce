eb3d4499eda5e5515885ebd6fe20c50c11e2b24c
ElAtrash <bioelatrash@gmail.com>
2020-07-05 02:55:43 +0300

    set cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   include Pundit
+  before_action :set_cart
 
   def pundit_user
     current_account
+  end
+
+  private
+
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
   end
 end
